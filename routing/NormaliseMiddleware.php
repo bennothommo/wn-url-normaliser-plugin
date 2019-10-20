a4cc0af4e6e700fc14501abcfcfee1df1b16fad9
Ben Thomson <git@alfreido.com>
2019-10-20 22:31:21 +0800

    Refactor middleware to use helper class

--- routing/NormaliseMiddleware.php
+++ routing/NormaliseMiddleware.php
@@ -2,8 +2,7 @@
 namespace BennoThommo\UrlNormaliser\Routing;
 
 use App;
-use BennoThommo\UrlNormaliser\Models\Settings;
-use Config;
+use BennoThommo\UrlNormaliser\Classes\Normalise;
 use Closure;
 
 class NormaliseMiddleware
@@ -26,88 +25,14 @@ class NormaliseMiddleware
             return $next($request);
         }
 
-        $settings = Settings::instance();
-        $urlInfo = parse_url($request->fullUrl());
-        $domain = $urlInfo['host'];
-        $requestPath = $request->getPathInfo();
-        $changed = false;
+        $originalUrl = preg_replace('/\?.*/', '', $request->getUri());
+        $normalisedUrl = Normalise::url($originalUrl);
 
-        // Determine paths to ignore
-        if (!empty($settings->ignore)) {
-            $ignorePaths = array_merge($this->ignore, preg_split('/[\r\n]+/', $settings->ignore, -1, PREG_SPLIT_NO_EMPTY));
-        } else {
-            $ignorePaths = $this->ignore;
-        }
-
-        if ($request->method() === 'GET') {
-            // Check if URL is ignored
-            if (count($ignorePaths) && $requestPath !== '/') {
-                $ignored = false;
-
-                foreach ($ignorePaths as $ignorePath) {
-                    if ($request->is($ignorePath)) {
-                        $ignored = true;
-                        break;
-                    }
-                }
-
-                if ($ignored) {
-                    return $next($request);
-                }
-            }
-
-            // Add or remove trailing slash if preferenced
-            if ($settings->trailing_slash !== 'none') {
-                // Do not apply trailing slash rules if the URL has an extension
-                $extension = pathinfo($requestPath, PATHINFO_EXTENSION);
-                $hasSlash = (preg_match('/\/$/', $requestPath) === 1);
-
-                if (empty($extension) && $requestPath !== '/') {
-                    if ($settings->trailing_slash === 'yes' && $hasSlash === false) {
-                        $requestPath = $requestPath . '/';
-                        $changed = true;
-                    }
-                    if ($settings->trailing_slash === 'no' && $hasSlash === true) {
-                        $requestPath = preg_replace('/\/+$/', '', $requestPath);
-                        $changed = true;
-                    }
-                }
-            }
-
-            // Add or remove www prefix if preferenced
-            if ($settings->www_prefix !== 'none') {
-                $hasPrefix = (preg_match('/^www./i', $domain) === 1);
-
-                if ($settings->www_prefix === 'www' && $hasPrefix === false) {
-                    $domain = 'www.' . $domain;
-                    $changed = true;
-                }
-                if ($settings->www_prefix === 'notWww' && $hasPrefix === true) {
-                    $domain = preg_replace('/^www./i', '', $domain);
-                    $changed = true;
-                }
-            }
-
-            // Add HTTPS if it is forced
-            if (empty($urlInfo['scheme'])) {
-                $urlInfo['scheme'] = 'https';
-            }
-            if (boolval($settings->force_https) === true && $urlInfo['scheme'] === 'http') {
-                $urlInfo['scheme'] = 'https';
-                $changed = true;
-            }
-
-            if ($changed === true) {
-                if ($settings->mode === 'redirect') {
-                    return redirect()->away(
-                        $urlInfo['scheme'] . '://' . $domain . $requestPath .
-                        ((!empty($urlInfo['query'])) ? '?' . $urlInfo['query'] : '') .
-                        ((!empty($urlInfo['fragment'])) ? '#' . $urlInfo['fragment'] : ''), 301);
-                } else {
-                    \BennoThommo\Meta\Link::set('canonical', $urlInfo['scheme'] . '://' . $domain . $requestPath .
-                    ((!empty($urlInfo['query'])) ? '?' . $urlInfo['query'] : '') .
-                    ((!empty($urlInfo['fragment'])) ? '#' . $urlInfo['fragment'] : ''));
-                }
+        if ($originalUrl !== $normalisedUrl) {
+            if (Normalise::doRedirect()) {
+                return redirect()->away($normalisedUrl, 301);
+            } else {
+                \BennoThommo\Meta\Link::set('canonical', $normalisedUrl);
             }
         }
 
